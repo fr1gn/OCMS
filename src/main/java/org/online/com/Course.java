3efceae7647a02a5575ea5e9284e8f01a49f62ec
Tairlan <231151@astanait.edu.kz>
2024-02-07 09:06:39 +0600

    OCMS

--- src/main/java/org/online/com/Course.java
+++ src/main/java/org/online/com/Course.java
@@ -1,4 +1,38 @@
 package org.online.com;
 
+import java.time.LocalDate;
+
 public class Course {
+    private int courseId;
+    private String courseName;
+    private String instructor;
+    private String description;
+    private LocalDate startDate;
+    private LocalDate endDate;
+
+    public int getCourseId() {
+        return courseId;
+    }
+
+
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public String getInstructor() {
+        return instructor;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
 }
