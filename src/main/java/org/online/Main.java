956068cdf36dbe107cca82b72c79df10b7457140
User <231608@astanait.edu.kz>
2024-02-07 09:15:52 +0600

    changes

--- src/main/java/org/online/Main.java
+++ src/main/java/org/online/Main.java
@@ -6,7 +6,7 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-
+//main
 public class Main {
     public static void main(String[] args) {
         String conString = "jdbc:postgresql://localhost:5432/simpledb";
