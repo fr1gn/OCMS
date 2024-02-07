304252a98055f2210402744dabbf193357a4caa9
Tairlan <231151@astanait.edu.kz>
2024-02-07 08:25:56 +0600

    started

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Main {
+    public static void main(String[] args) {
+        String conString = "jdbc:postgresql://localhost:5432/simpledb";
+        Connection con = null;
+        Statement statement = null;
+        ResultSet rs = null;
+
+        try {
+            con = DriverManager.getConnection(conString, "postgres", "231608");
+            statement = con.createStatement();
+            rs = statement.executeQuery("SELECT name, surname, age, gender, course FROM users");
+        } catch (SQLException e) {
+            System.out.println("connection error:" + e.getMessage());;
+        }
+
+    }
+}
