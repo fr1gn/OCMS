e7bbd64624c9b28ef0d77998b8f97e56a41f29ed
Tamer <232146@astanait.edu.kz>
2024-02-11 13:54:41 +0600

    OCMS

--- src/main/java/org/online/repesitories/interfaces/IUserRepository.java
+++ src/main/java/org/online/repesitories/interfaces/IUserRepository.java
@@ -1,4 +1,10 @@
 package org.online.repesitories.interfaces;
+import org.online.com.User;
+
 import java.util.List;
 public interface IUserRepository {
+    // default operations for IUserrepository
+    User getUserById(int userId);
+    List<User> getAllUsers();
+
 }
