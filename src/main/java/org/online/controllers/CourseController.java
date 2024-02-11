f4df0a0d021c013a0ba34acd98a5ab616d77d00a
User <231608@astanait.edu.kz>
2024-02-11 17:31:57 +0600

    Course controller

--- src/main/java/org/online/controllers/CourseController.java
+++ src/main/java/org/online/controllers/CourseController.java
@@ -1,4 +1,30 @@
 package org.online.controllers;
 
+import org.online.com.Course;
+import org.online.repesitories.interfaces.ICourseRepository;
+
+import java.util.List;
+
 public class CourseController {
+    private final ICourseRepository repo;
+
+    public CourseController(ICourseRepository repo) {
+        this.repo = repo;
+    }
+
+    public String getCourse(int id) {
+        Course course = repo.getCourseById(id);
+
+        return (course == null ? "Course was not found!" : course.toString());
+    }
+    public String getAllCourses() {
+        List<Course> courses = repo.getAllCourses();
+
+        StringBuilder response = new StringBuilder();
+        for (Course course : courses) {
+            response.append(course.toString()).append("\n");
+        }
+
+        return response.toString();
+    }
 }
--- src/main/java/org/online/repesitories/interfaces/ICourseRepository.java
+++ src/main/java/org/online/repesitories/interfaces/ICourseRepository.java
@@ -6,4 +6,5 @@ import java.util.List;
 public interface ICourseRepository {
     Course getCourseById(int courseId);
     List<Course> getAllCourses();
+
 }
