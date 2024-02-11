c85e8e910b0618e1c8c6b0dfa785337504905350
User <231608@astanait.edu.kz>
2024-02-11 17:22:07 +0600

    CourseRepository

--- src/main/java/org/online/repesitories/CourseRepository.java
+++ src/main/java/org/online/repesitories/CourseRepository.java
@@ -1,4 +1,82 @@
 package org.online.repesitories;
 
-public class CourseRepository {
+import org.online.com.Course;
+import org.online.data.interfaces.IDB;
+import org.online.repesitories.interfaces.ICourseRepository;
+import java.sql.*;
+import java.util.LinkedList;
+import java.util.List;
+
+public class CourseRepository implements ICourseRepository {
+    private final IDB db;  // Dependency Injection
+    public CourseRepository(IDB db) {
+        this.db = db;
+    }
+
+    @Override
+    public Course getCourseById(int courseId) {
+        Connection con = null;
+
+        try {
+            con = db.getConnection();
+            String sql = "SELECT courseId, courseName, instructor, description FROM courses WHERE id=?";
+            PreparedStatement st = con.prepareStatement(sql);
+
+            st.setInt(1, courseId);
+
+            ResultSet rs = st.executeQuery();
+            if (rs.next()) {
+                return new Course(rs.getInt("courseId"),
+                        rs.getString("courseName"),
+                        rs.getString("instructor"),
+                        rs.getString("description"));
+            }
+        } catch (SQLException e) {
+            System.out.println("sql error: " + e.getMessage());
+        } finally {
+            try {
+                if (con != null)
+                    con.close();
+            } catch (SQLException e) {
+                System.out.println("sql error: " + e.getMessage());
+            }
+        }
+
+        return null;
+    }
+
+    @Override
+    public List<Course> getAllCourses() {
+        Connection con = null;
+
+        try {
+            con = db.getConnection();
+            String sql = "SELECT courseId, courseName, instructor, description FROM courses";
+            Statement st = con.createStatement();
+
+            ResultSet rs = st.executeQuery(sql);
+            List<Course> users = new LinkedList<>();
+            while (rs.next()) {
+                Course user = new Course(rs.getInt("courseId"),
+                        rs.getString("courseName"),
+                        rs.getString("instructor"),
+                        rs.getString("description"));
+
+                users.add(user);
+            }
+
+            return users;
+        } catch (SQLException e) {
+            System.out.println("sql error: " + e.getMessage());
+        } finally {
+            try {
+                if (con != null)
+                    con.close();
+            } catch (SQLException e) {
+                System.out.println("sql error: " + e.getMessage());
+            }
+        }
+
+        return null;
+    }
 }
--- src/main/java/org/online/repesitories/interfaces/ICourseRepository.java
+++ src/main/java/org/online/repesitories/interfaces/ICourseRepository.java
@@ -1,4 +1,9 @@
 package org.online.repesitories.interfaces;
+import org.online.com.Course;
+import org.online.com.Enrollment;
+
 import java.util.List;
 public interface ICourseRepository {
+    Course getCourseById(int courseId);
+    List<Course> getAllCourses();
 }
