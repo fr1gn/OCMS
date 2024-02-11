c75198756bfe748695a485d9ab8d55d62e07c27c
Tairlan <231151@astanait.edu.kz>
2024-02-11 13:11:02 +0600

    OCMS

--- src/main/java/org/online/data/PostgresDB.java
+++ src/main/java/org/online/data/PostgresDB.java
@@ -1,4 +1,24 @@
 package org.online.data;
 
-public class PostgresDB {
+import org.online.data.interfaces.IDB;
+import java.sql.*;
+
+public class PostgresDB implements IDB {
+    @Override
+    public Connection getConnection() {
+        String connectionUrl = "jdbc:postgresql://localhost:5432/simpledb";
+        try {
+            // Here we load the driver’s class file into memory at the runtime
+            Class.forName("org.postgresql.Driver");
+
+            // Establish the connection
+            Connection con = DriverManager.getConnection(connectionUrl, "postgres", "asdfg1020A");
+
+            return con;
+        } catch (Exception e) {
+            System.out.println("failed to connect to postgres: " + e.getMessage());
+
+            return null;
+        }
+    }
 }
