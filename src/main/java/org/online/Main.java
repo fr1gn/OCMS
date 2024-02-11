13ef9d33507992cb896ed3946424aef0baed76a0
Tairlan <231151@astanait.edu.kz>
2024-02-11 13:14:17 +0600

    OCMS

--- src/main/java/org/online/Main.java
+++ src/main/java/org/online/Main.java
@@ -9,18 +9,6 @@ import java.sql.Statement;
 //main
 public class Main {
     public static void main(String[] args) {
-        String conString = "jdbc:postgresql://localhost:5432/simpledb";
-        Connection con = null;
-        Statement statement = null;
-        ResultSet rs = null;
-
-        try {
-            con = DriverManager.getConnection(conString, "postgres", "231608");
-            statement = con.createStatement();
-            rs = statement.executeQuery("SELECT name, surname, course FROM users");
-        } catch (SQLException e) {
-            System.out.println("connection error:" + e.getMessage());;
-        }
 
     }
 }
