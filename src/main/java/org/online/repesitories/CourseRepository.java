1448311974438c5ffdbc655f56d4887c03794c4d
Tamer <232146@astanait.edu.kz>
2024-02-11 23:28:41 +0600

    OCMS

--- src/main/java/org/online/repesitories/CourseRepository.java
+++ src/main/java/org/online/repesitories/CourseRepository.java
@@ -8,7 +8,7 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class CourseRepository implements ICourseRepository {
-    private final IDB db;  // Dependency Injection
+   private final IDB db;  // Dependency Injection
     public CourseRepository(IDB db) {
         this.db = db;
     }
--- src/main/java/org/online/repesitories/UserRepository.java
+++ src/main/java/org/online/repesitories/UserRepository.java
@@ -1,12 +1,26 @@
 package org.online.repesitories;
 
 import com.sun.jdi.connect.spi.Connection;
+import org.online.com.Course;
+import java.sql.*;
+import java.util.LinkedList;
+import java.util.List;
+import org.online.data.interfaces.IDB;
 import org.online.com.User;
+import org.online.data.interfaces.IDB;
 import org.online.repesitories.interfaces.IUserRepository;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 public class UserRepository implements IUserRepository {
+    private final IDB db;  // Dependency Injection
+    public CourseRepository(IDB db) {
+        this.db = db;
+    }
+}
 
 
 
@@ -19,7 +33,35 @@ public class UserRepository implements IUserRepository {
 
         @Override
     public User getUser(int userId) {
-        return null;
+            java.sql.Connection con = null;
+
+            try {s
+                con = db.getConnection();
+                String sql = "SELECT userId, firstName, lastName, email, password, role FROM users WHERE userid=?";
+                PreparedStatement st = con.prepareStatement(sql);
+
+                st.setInt(1, userId);
+
+                ResultSet rs = st.executeQuery();
+                if (rs.next()) {
+                    return new user(rs.getInt("userId"),
+                            rs.getString("firstName"),
+                            rs.getString("lastName"),
+                            rs.getString("email"),
+                            rs.getString("password"),
+                            rs.getString("role"));
+                }
+            } catch (SQLException e) {
+                System.out.println("sql error: " + e.getMessage());
+            } finally {
+                try {
+                    if (con != null)
+                        con.close();
+                } catch (SQLException e) {
+                    System.out.println("sql error: " + e.getMessage());
+                }
+            }
+            return null;
     }
 
     @Override
