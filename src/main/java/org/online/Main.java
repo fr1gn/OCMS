a19aa97b9fac44d2986a1093d46b46d9c241a496
Tairlan <231151@astanait.edu.kz>
2024-02-13 14:20:12 +0600

    OCMS

--- src/main/java/org/online/Main.java
+++ src/main/java/org/online/Main.java
@@ -4,9 +4,31 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import org.online.data.interfaces.IDB;
+import org.online.data.PostgresDB;
+import org.online.repesitories.interfaces.IUserRepository;
+import org.online.repesitories.UserRepository;
+import org.online.controllers.UserController;
+import org.online.repesitories.interfaces.ICourseRepository;
+import org.online.repesitories.CourseRepository;
+import org.online.controllers.CourseController;
+import org.online.repesitories.interfaces.IEnrollmentRepository;
+import org.online.repesitories.EnrollmentRepository;
+import org.online.controllers.EnrollmentController;
 //main
 public class Main {
     public static void main(String[] args) {
+
+        IDB db = new PostgresDB();
+        IUserRepository repo = new UserRepository(db);
+        UserController controller = new UserController(repo);
+        ICourseRepository repo1 = new CourseRepository(db);
+        CourseController controller1 = new CourseController(repo1);
+        IEnrollmentRepository repo2 = new EnrollmentRepository(db);
+        EnrollmentController controller2 = new EnrollmentController(repo2);
+
+        MyApplication app = new MyApplication(controller, controller1,controller2);
+        app.start();
 
     }
 }
