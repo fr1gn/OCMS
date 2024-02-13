45e42ffe727930924b1d5cc515a7018a8dcfe514
Tairlan <231151@astanait.edu.kz>
2024-02-13 12:57:45 +0600

    OCMS

--- src/main/java/org/online/controllers/EnrollmentController.java
+++ src/main/java/org/online/controllers/EnrollmentController.java
@@ -1,2 +1,27 @@
-package org.online.controllers;public class EnrollmentController {
+package org.online.controllers;
+import org.online.com.Enrollment;
+import org.online.repesitories.interfaces.IEnrollmentRepository;
+import java.util.List;
+public class EnrollmentController {
+    private final IEnrollmentRepository repo;
+
+    public EnrollmentController(IEnrollmentRepository repo) {
+        this.repo = repo;
+    }
+
+    public String enrollUserInCourse(int userId, int courseId) {
+        repo.enrollUserInCourse(userId, courseId);
+        return "User successfully enrolled in the course";
+    }
+
+    public String getAllEnrollments() {
+        List<Enrollment> enrollments = repo.getAllEnrollments();
+        StringBuilder response = new StringBuilder();
+        for (Enrollment enrollment : enrollments) {
+            response.append(enrollment.toString()).append("\n");
+        }
+
+        return response.toString();
+    }
+
 }
